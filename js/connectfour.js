71291dfc9bf40decb5d9947822cc5a63fd46ccce
Vianney Chin <vianneychin@Vianneys-MacBook-Pro.local>
2019-05-05 19:23:32 -0700

    Add winner name functionality with click as well

--- js/connectfour.js
+++ js/connectfour.js
@@ -1,56 +1,61 @@
 const CLICK_start_to_continue =
- $('.start-button').click( () =>
-    $('.how-to-play-container').css("display", 'none')
-    &&
-    $(".enter-player-one-name").css('display', 'inline-flex')
+    $('.start-button').click( () =>
+        $('.how-to-play-container').css("display", 'none')
+        &&
+        $(".enter-player-one-name").css('display', 'inline-flex')
     )
 const CLICK_enter_to_continue_for_player_one =
-$('.enter-player-one').click( () => {
-    $('.enter-player-one-name').css("display", 'none') &&
-    $('.enter-player-two-name').css('display', 'inline-flex')
-})
+    $('.enter-player-one').click( () => {
+        $('.enter-player-one-name').css("display", 'none') &&
+        $('.enter-player-two-name').css('display', 'inline-flex')
+    })
 const CLICK_enter_to_continue_for_player_two =
     $('.enter-player-two').click( () => {
-    $('.enter-player-two-name').css("display", 'none') &&
-    $('#connect-four-container').css('display', 'inline-flex')
-})
+        $('.enter-player-two-name').css("display", 'none') &&
+        $('#connect-four-container').css('display', 'inline-flex')
+    })
 const KEYPRESS_enter_to_continue_for_player_one =
- $('.player-one-input').on('keypress', (event => {
-    if(event.which == 13) {
-        $('.enter-player-one').click()
-    }
-}))
-const KEYPRESS_enter_to_continue_for_player_two =
- $('.player-two-input').on('keypress', (event => {
-    if(event.which == 13) {
-        $('.enter-player-two').click()
-    }
-}))
-const DISPLAY_player_one_name = 
-    $('.player-one-input').keyup( () => {
-    let name = $('.player-one-input').val()
-    playerArray.push(name)
-    Name = playerArray.pop()
-    $('.player-one-name').text(name)
     $('.player-one-input').on('keypress', (event => {
         if(event.which == 13) {
-            playerArray.push(Name)
-            const unique = [...new Set (playerArray)]
+            $('.enter-player-one').click()
         }
     }))
-})
-const DISPLAY_player_two_name = 
-$('.player-two-input').keyup( () => {
-    let name = $('.player-two-input').val()
-    playerArray.push(name)
-    Name = playerArray.pop()
-    $('.player-two-name').text(name)
+const KEYPRESS_enter_to_continue_for_player_two =
     $('.player-two-input').on('keypress', (event => {
         if(event.which == 13) {
-            playerArray.push(Name)
+            $('.enter-player-two').click()
         }
     }))
-})
+const DISPLAY_player_one_name = 
+    $('.player-one-input').keyup( () => {
+        let name = $('.player-one-input').val()
+        playerNames.push(name)
+        Name = playerNames.pop()
+        $('.player-one-name').text(name)
+        $('.player-one-input').on('keypress', (event => {
+            if(event.which == 13) {
+                playerNames.push(Name)
+            }
+        }))
+        $('.enter-player-one').click( () => {
+            playerNames.push(Name)
+        })
+    })
+const DISPLAY_player_two_name = 
+    $('.player-two-input').keyup( () => {
+        let name = $('.player-two-input').val()
+        playerNames.push(name)
+        Name = playerNames.pop()
+        $('.player-two-name').text(name)
+        $('.player-two-input').on('keypress', (event => {
+            if(event.which == 13) {
+                playerNames.push(Name)
+            }
+        }))
+        $('.enter-player-two').click( () => {
+            playerNames.push(Name)
+        })
+    })
 const game = {
     currentPlayer:{},
     players:[
@@ -58,22 +63,24 @@ const game = {
         {player:"two", color: "rgb(193, 219, 220)"}
     ]
 }
-const   playerArray = []
+const playerNames = []
 const      $columns = $(`.board-column`)
 
 
 const     setPlayer = () => game.currentPlayer = game.players[0]
           setPlayer()
-const     defaultPlayer = () => $('.border-one').css("border", "solid 10px #bbc687")
-          defaultPlayer()
+const defaultPlayer = () => $('.border-one').css("border", "solid 10px #bbc687")
+      defaultPlayer()
 
 const   checkWinner = () => {
+    const array = [...new Set(playerNames)]
+    if (game.currentPlayer === game.players[0]) {
+        $('.winner').text(`${array[0]} WINS!!!`)
+    } else {
+        $('.winner').text(`${array[1]} WINS!!!`)
+    }
     $('.winner-declaration').css('display', 'inline')
     $('#connect-four-game-board').css('pointer-events', 'none')
-    if (checkWinner) {
-        $('.winner').text(`${playerArray[1]} WINS!!!`)
-        // console.log([...new Set(playerArray)])
-    }
 }
 const switchPlayers = () => {
     const playerOne = game.players[0]
@@ -94,17 +101,8 @@ const checkVertical = (count) => {
         }
     }
 }
-const switch_default_player = $columns.click( () => {
-    if (game.currentPlayer === game.players[1]) {
-        $('.border-two').css("border", "solid 10px #c1dbdc")
-        $('.border-one').css("border", "")
-    } else if (game.currentPlayer === game.players[0]) {
-        $('.border-two').css("border", "")
-        $('.border-one').css("border", "solid 10px #bbc687")
-    }
-})
-$columns.click((e => {
-    let $column = $(e.currentTarget).children()
+const  GAME_START = $columns.click((e => {
+    const $column = $(e.currentTarget).children()
     for (let i  = $(e.currentTarget).children().length - 1; i >= 0; i--) {
             if ($(e.currentTarget).children()[i].style.backgroundColor === "") {
                 $(e.currentTarget).children()[i].style.backgroundColor = game.currentPlayer.color
@@ -118,3 +116,12 @@ $columns.click((e => {
         }
     }
 ))
+const SWITCH_DEFAULT_OFF = $columns.click( () => {
+    if (game.currentPlayer === game.players[1]) {
+        $('.border-two').css("border", "solid 10px #c1dbdc")
+        $('.border-one').css("border", "")
+    } else if (game.currentPlayer === game.players[0]) {
+        $('.border-two').css("border", "")
+        $('.border-one').css("border", "solid 10px #bbc687")
+    }
+})
